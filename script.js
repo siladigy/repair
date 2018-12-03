d19ddb9838396eb658c6c90b1a18184bca976cd9
George Siladi <sgeorge7731@gmail.com>
2018-12-03 11:12:03 +0200

    добавлена секция с портфолио

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ $(document).ready(function(){
     });
    
 });
+
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow arrow_right"></div>',
+    prevArrow: '<div class="arrow arrow_left"></div>',
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+    ]
+  });
+});
